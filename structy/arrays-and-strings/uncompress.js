2e1fd8d0b4161dfba5ad3a0328ccfef2c87f3f32
Aaron Sanchez <saarong12@gmail.com>
2024-09-04 20:56:09 -0500

    Rewrote a solution to use the two pointer method.

--- structy/arrays-and-strings/uncompress.js
+++ structy/arrays-and-strings/uncompress.js
@@ -5,33 +5,59 @@
 // You may assume that the input string is well-formed according to the previously mentioned pattern.
 
 // MY WORK
-const uncompress = (s) => {
-  let currentVal = ''
-  let newS = ''
-  // loop through s
-  for (let i = 0; i < s.length; i++) {
-    // collect count of how many times char should be repeated
-    if (parseInt(s[i])) {
-      currentVal += s[i]
-    } else {
-      // if value returns NaN we found our char
-      // add the char repeating `currentCount` times to the newS
-      newS += s[i].repeat(parseInt(currentVal))
-      // reset the currentVal
-      currentVal = ''
-    }
-  }
-  return newS
-}
+// const uncompress = (s) => {
+//   let currentVal = ''
+//   let newS = ''
+//   // loop through s
+//   for (let i = 0; i < s.length; i++) {
+//     // collect count of how many times char should be repeated
+//     if (parseInt(s[i])) {
+//       currentVal += s[i]
+//     } else {
+//       // if value returns NaN we found our char
+//       // add the char repeating `currentCount` times to the newS
+//       newS += s[i].repeat(parseInt(currentVal))
+//       // reset the currentVal
+//       currentVal = ''
+//     }
+//   }
+//   return newS
+// }
+// // Works decent but += concatenation is O(n) runtime.
 
-console.log(uncompress('2c3a1t')) // ccaaat
-console.log(uncompress('4s2b')) // ssssbb
-console.log(uncompress('2p1o5p')) // ppoppppp
+// console.log(uncompress('2c3a1t')) // ccaaat
+// console.log(uncompress('4s2b')) // ssssbb
+// console.log(uncompress('2p1o5p')) // ppoppppp
 
 // COMPLEXITY GUESSES
 // n = s.length
 // Time = O(n)
 // Space = O(1)
+
+// Re-worked using two pointers
+const betterUncompress = (s) => {
+  const nums = '0123456789'
+  const newS = []
+  let i = 0
+  let j = 0
+  while (j < s.length) {
+    if (nums.includes(s[j])) {
+      j++
+    } else {
+      const num = Number(s.slice(i, j))
+      for (let count = 0; count < num; count++) {
+        newS.push(s[j])
+      }
+      j++
+      i = j
+    }
+  }
+  return newS.join('')
+}
+
+console.log(betterUncompress('2c3a1t')) // ccaaat
+console.log(betterUncompress('4s2b')) // ssssbb
+console.log(betterUncompress('2p1o5p')) // ppoppppp
 
 // WALKTHROUGH SOLUTION
 // const uncompress = (s) => {
