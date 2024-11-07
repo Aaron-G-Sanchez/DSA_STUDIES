ccc8067c50e4e800d9d9eec36799436c59b73e86
Aaron Sanchez <saarong12@gmail.com>
2024-11-06 23:22:53 -0600

    Added solution to reverseWords leetcode problem.

---
+++ leetcode/reverseWords.js
@@ -0,0 +1,28 @@
+// 151. Reverse words in a string
+
+// A word is defined as a sequence of non-space characters.
+
+// The words in `s` will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words.
+// The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+const reverseWords = (s) => {
+  let trimmed = s.split(' ')
+
+  let res = []
+
+  for (let i = trimmed.length - 1; i >= 0; i--) {
+    if (trimmed[i] != '') {
+      res.push(trimmed[i])
+    }
+  }
+
+  return res
+}
+
+// console.log(reverseWords('the sky is blue')) // -> "blue is sky the"
+console.log(reverseWords('   hello world    ')) // -> "word hello"
+// console.log(reverseWords('a good   example')) // -> "example good a"
