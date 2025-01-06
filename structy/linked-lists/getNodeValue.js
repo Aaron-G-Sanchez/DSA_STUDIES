a3df49aab36d456d1a68c3455c9b6cc2cc9a37ff
Aaron Sanchez <saarong12@gmail.com>
2025-01-06 14:03:55 -0600

    Added recursive solution to structy problem getNodeValue.

--- structy/linked-lists/getNodeValue.js
+++ structy/linked-lists/getNodeValue.js
@@ -15,26 +15,52 @@ a.next = b
 b.next = c
 c.next = d
 
-const getNodeValue = (head, index) => {
-  // initialize a count
-  let count = 0
-  // initialize a current node
-  let current = head
-  // while current != null
-  while (current) {
-    // check if the count is equal to the desired index
-    // if yes return current.val
-    if (count === index) return current.val
-    // if no
-    // increment count
-    count++
-    // set current equal to the next node in the list for the next iteration
-    current = current.next
-  }
+// Iterative solution
+// const getNodeValue = (head, index) => {
+//   // initialize a count
+//   let count = 0
+//   // initialize a current node
+//   let current = head
+//   // while current != null
+//   while (current) {
+//     // check if the count is equal to the desired index
+//     // if yes return current.val
+//     if (count === index) return current.val
+//     // if no
+//     // increment count
+//     count++
+//     // set current equal to the next node in the list for the next iteration
+//     current = current.next
+//   }
 
-  // return null if we make it through the entire list
-  return null
+//   // return null if we make it through the entire list
+//   return null
+// }
+
+// n = number of nodes
+// Time = O(n)
+// Space = O(1)
+
+// Recursive solution.
+const getNodeValue = (head, index) => {
+  // Call helper function
+  return getNode(head, index, 0)
 }
 
+const getNode = (head, index, count) => {
+  // base case
+  // check if end of the list has been reached
+  if (!head) return null
+  // check if index equals the nodes encountered
+  if (index === count) return head.val
+  // iterate to the next node by calling getNode on the node.next
+  return getNode(head.next, index, ++count)
+}
+
+// n = number of nodes
+// Time = O(n)
+// Space = O(n)
+
 // console.log(getNodeValue(a, 2)) // 'c'
-console.log(getNodeValue(a, 6)) // null
+// console.log(getNodeValue(a, 6)) // null
+console.log(getNodeValue(a, 0)) // 'a'
