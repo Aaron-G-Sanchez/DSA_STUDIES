012058bfc83fcf6b30adf63974cd2760c413a831
Aaron Sanchez <saarong12@gmail.com>
2025-01-06 21:33:49 -0600

    Added recursive solution to structy problem reverseList.

--- structy/linked-lists/reverseList.js
+++ structy/linked-lists/reverseList.js
@@ -16,34 +16,52 @@ b.next = c
 c.next = d
 d.next = e
 
-// Iterative solution
-const reverseList = (head) => {
-  // save the current node
-  let current = head
-  // save previous node
-  let prev = null
+// // Iterative solution
+// const reverseList = (head) => {
+//   // save the current node
+//   let current = head
+//   // save previous node
+//   let prev = null
 
-  let next = current.next
+//   let next = current.next
 
-  // while node does not equal null
-  // loop through list
-  while (current) {
-    // set the current.next val to previous
-    current.next = prev
-    // set previous = current
-    prev = current
-    // set current = next
-    current = next
-    // move to the next node
-    if (current) {
-      next = current.next
-    }
-  }
-  // return the final node or the new head of the list
-  return prev
-}
+//   // while node does not equal null
+//   // loop through list
+//   while (current) {
+//     // set the current.next val to previous
+//     current.next = prev
+//     // set previous = current
+//     prev = current
+//     // set current = next
+//     current = next
+//     // move to the next node
+//     if (current) {
+//       next = current.next
+//     }
+//   }
+//   // return the final node or the new head of the list
+//   return prev
+// }
 
 // console.log('solution:', reverseList(a)) // e
+
+const reverseList = (head) => {
+  return reverseHelper(head, null)
+}
+
+const reverseHelper = (current, prev) => {
+  // base case
+  // we reach the end of the list
+  if (!current) return prev
+
+  // define the next node in the list
+  let next = current.next
+  // assign the current node a new .next val
+  current.next = prev
+
+  // recursively call the helper with the new current and previous nodes
+  return reverseHelper(next, current)
+}
 
 const printList = (head) => {
   let current = head
