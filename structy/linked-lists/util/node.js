fdf460d11f5566c29e32109bc7f8a5aa7a79fcf6
Aaron Sanchez <saarong12@gmail.com>
2025-01-21 22:11:37 -0600

    Added print function to the Node class in the structy util directory.

--- structy/linked-lists/util/node.js
+++ structy/linked-lists/util/node.js
@@ -3,6 +3,15 @@ class Node {
     this.val = val
     this.next = null
   }
+
+  /** Function to list all subsequent nodes. */
+  printList() {
+    let current = this
+    while (current) {
+      console.log(current.val)
+      current = current.next
+    }
+  }
 }
 
 module.exports = {
