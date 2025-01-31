0cc186a4a67dd8cef5443313696744e9dd5e949d
Aaron Sanchez <saarong12@gmail.com>
2025-01-31 00:10:21 -0600

    Added solution to Leetcode problem 35. Search Inset Position

---
+++ leetcode/searchInsertPosition.js
@@ -0,0 +1,43 @@
+// 35. Search Insert Position
+
+// Gien a sorted array of distinct integers and a target value,
+// return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+const searchInsert = (nums, target) => {
+  // Need to use a binary search algorithm.
+
+  // Initialize two pointers to hold the start and endpoint
+  let start = 0
+  let end = nums.length - 1
+  let mid
+  // Loop until the start and end points are pointing to each other
+  while (start <= end) {
+    // Find the mid point (start + end / 2)
+    mid = Math.floor((start + end) / 2)
+
+    // Evaluate if the target is less than or greater than the target
+    if (nums[mid] === target) {
+      return mid
+    } else if (nums[mid] < target) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+  // return the midpoint value
+  console.log(' mid:', nums[mid])
+  console.log(' start:', nums[start])
+  console.log(' end:', nums[end])
+  console.log('Target:', target)
+
+  if (mid === start) {
+    return mid
+  } else {
+    return ++mid
+  }
+}
+
+const testArr = [1, 3, 5, 6]
+console.log(searchInsert(testArr, 0))
